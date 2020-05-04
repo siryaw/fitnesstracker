560ad0adbe9c0095866d341a6021be879d58ecd2
Sir Yaw <samappjnr@aol.com>
2020-05-03 22:55:33 -0400

    ...

---
+++ models/workouts.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: { type: Date, required: true },
+  exercises: [
+    {
+      type: {
+        type: String
+      },
+      name: {
+        type: String
+      },
+      duration: {
+        type: Number
+      },
+      weight: {
+        type: Number
+      },
+      reps: {
+        type: Number
+      },
+      sets: {
+        type: Number
+      }
+    }
+  ]
+});
+
