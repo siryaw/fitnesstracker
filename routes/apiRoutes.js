c3587280f8cbefffb5744bea3e4cfd07d6d3e46c
Sir Yaw <samappjnr@aol.com>
2020-05-04 15:40:33 -0400

    code for serverJs

---
+++ routes/apiRoutes.js
@@ -0,0 +1,42 @@
+const router = require("express").Router();
+const Workout = require("../models").Workouts;
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then(workouts => res.json(workouts))
+    .catch(err => res.json(err));
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(
+    req.params.id,
+    { $push: { exercises: req.body } },
+    { new: true }
+  )
+    .then(workout => res.json(workout))
+    .catch(err => res.json(err));
+});
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create({
+    day: Date.now()
+  })
+    .then(newWorkout => {
+      console.log("o am the cretead worrkout: ", newWorkout);
+      res.json(newWorkout);
+    })
+    .catch(err => res.json(err));
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .then(workouts => {
+      res.json(workouts);
+    })
+    .catch(err => res.json(err));
+});
+
+router.delete("/api/workouts", (req, res) => {
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(
+  "mongodb+srv://user:user@mycluster-i5mnv.mongodb.net/workouts?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  }
+;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.use(apiRoutes);
+app.use(viewRoutes);
+
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.listen(PORT, () => console.log("listening on port: ", PORT));
